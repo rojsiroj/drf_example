0cdedf383255f71b94cca9c48fd6d7e77953aa4a
rojsiroj <sirojudin.dev@gmail.com>
2024-06-26 14:42:22 +0700

    chore: add utils, add core model/admin/view/serializer, and update docker composer

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from utils.abstract_model import AbstractModel
 
-# Create your models here.
+from django_extensions.db.models import (
+	TimeStampedModel, 
+	ActivatorModel,
+	TitleDescriptionModel
+)
+
+class Contact(
+	TimeStampedModel, 
+	ActivatorModel,
+	TitleDescriptionModel,
+	AbstractModel
+	):
+
+	class Meta:
+		verbose_name_plural = "Contacts"
+
+	email = models.EmailField(verbose_name="Email")
+
+	def __str__(self):
+		return f'{self.title}'
